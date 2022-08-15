33215c737a964151583760337565372eb27dfd64
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-08-15 22:59:28 +0300

    creating api. get token request

--- source/js/api.js
+++ source/js/api.js
@@ -1,8 +1,35 @@
-// const GET_DATA_URL = 'https://26.javascript.pages.academy/keksobooking/data';
+const GET_DATA_URL = 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200';
+const GET_TOKEN_URL = 'https://test.api.amadeus.com/v1/security/oauth2/token';
 
-// const getData = (onSuccess, onError) => {
-//   fetch(GET_DATA_URL)
-//     .then((response) => { response.ok ? response.json() : onError(); })
-//     .then((tickets) => { onSuccess(tickets); })
-//     .catch(() => console.log('Не удалось загрузить данные'))
-// };
+const setLocalStorage = (accessToken) => {
+  localStorage.setItem('Bearer', accessToken);
+};
+
+const getLocalStorage = () => {
+  return localStorage.getItem('Bearer');
+};
+
+const getAccessToken = () => {
+  fetch(GET_TOKEN_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials&client_id=NhVdSLuZzGuK9A2VTaYobkrHtAvnAwEe&client_secret=ZAHRlkckNqSA3nn6',
+  })
+  .then((response) => response.json())
+  .then((response) => response.access_token)
+  .then((access_token) => setLocalStorage(access_token))
+};
+
+const getApiRequest = () => {
+  fetch(GET_DATA_URL, {
+    headers: {
+      'Authorization': `Bearer ${getLocalStorage()}`,
+    },
+  })
+  .then((response) => response.json())
+};
+
+getAccessToken();
+getApiRequest();
